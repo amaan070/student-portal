30d1b5fd52ca3eb50e9ea6165156b1045f533a60
Amaan <pramn070@gmail.com>
2026-08-16 02:14:19 +0530

    Set dark mode default to disabled

--- settings.cpp
+++ settings.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-bool isDarkModeEnabled = true;
+bool isDarkModeEnabled = false;
 string language = "English";
 bool notificationsEnabled = true;
 int sessionTimeout = 30;
