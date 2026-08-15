849c98b90cd7600fdae7c7385491cc6989233ef2
Amaan <pramn070@gmail.com>
2026-08-16 01:06:27 +0530

    Added language preference setting

--- settings.cpp
+++ settings.cpp
@@ -2,9 +2,10 @@
 using namespace std;
 
 bool isDarkModeEnabled = true;
+string language = "English";
 
 int main() {
     	cout << "Dark mode: " << (isDarkModeEnabled ? "Enabled" : "Disabled") << endl;
-
+	cout << "Language: " << language << endl;
     return 0;
 }
