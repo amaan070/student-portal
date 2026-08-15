6dd274865651a8d874bdb1bce20cb342dce5820e
Amaan <pramn070@gmail.com>
2026-08-15 23:47:47 +0530

    Added logout feature.

--- login.cpp
+++ login.cpp
@@ -3,23 +3,29 @@
 using namespace std;
 
 bool validate(const string& username, const string& password) {
-    return username == "2026201032" && password == "portal123";
+	return username == "2026201032" && password == "portal123";
+}
+
+void logout() {
+	cout << "Logged out successfully." << '\n';
 }
 
 int main() {
-    string username, password;
+	string username, password;
 
-    cout << "Enter Username: ";
-    cin >> username;
+    	cout << "Enter Username: ";
+    	cin >> username;
 
-    cout << "Enter Password: ";
-    cin >> password;
+    	cout << "Enter Password: ";
+    	cin >> password;
 
-    if (validate(username, password)) {
-        cout << "Login successful." << '\n';
-    } else {
-        cout << "Invalid username or password." << '\n';
-    }
+    	if (validate(username, password)) {
+        	cout << "Login successful." << '\n';
+    	} else {
+        	cout << "Invalid username or password." << '\n';
+    	}
+	
+	logout();
 
-    return 0;
+    	return 0;
 }
