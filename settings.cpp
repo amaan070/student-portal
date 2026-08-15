2be953d40641310765a3beab26e5e339a13301c0
Amaan <pramn070@gmail.com>
2026-08-16 01:02:23 +0530

    Add dark mode option

---
+++ settings.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+bool isDarkModeEnabled = true;
+
+int main() {
+    	cout << "Dark mode: " << (isDarkModeEnabled ? "Enabled" : "Disabled") << endl;
+
+    return 0;
+}
