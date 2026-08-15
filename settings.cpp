a1bc9870e13e6a50de790ec66eeeae8e85a045ee
Amaan <pramn070@gmail.com>
2026-08-16 01:11:26 +0530

    Add notification preference setting

--- settings.cpp
+++ settings.cpp
@@ -3,9 +3,12 @@ using namespace std;
 
 bool isDarkModeEnabled = true;
 string language = "English";
+bool notificationsEnabled = true;
 
 int main() {
     	cout << "Dark mode: " << (isDarkModeEnabled ? "Enabled" : "Disabled") << endl;
 	cout << "Language: " << language << endl;
+
+	cout << "Notifications: " << (notificationsEnabled ? "Enabled" : "Disabled") << endl;
     return 0;
 }
